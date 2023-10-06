e70b2ca1c9705f16b3dc53466239a98fc300c995
Andrew Coffey <121702632+AndrewCoffey02@users.noreply.github.com>
2023-10-06 12:33:02 +0100

    added packages and navbars

--- Lab-2/book-app/src/App.js
+++ Lab-2/book-app/src/App.js
@@ -1,16 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
 import Footer from './components/Footer';
 import Content from './components/content';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <Header></Header>
+
+<Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="read">Read</Nav.Link>
+            <Nav.Link href="create">Create</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="/home" element={<Content></Content>}></Route>
+        <Route path="/read" element={<Header></Header>}></Route>
+        <Route path="/create" element={<Footer></Footer>}></Route>
+      </Routes>
+
+      {/* <Header></Header>
       <Content></Content>
-      <Footer></Footer>
+      <Footer></Footer> */}
     </div>
+    </BrowserRouter>
   );
 }
 
